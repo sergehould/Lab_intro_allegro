e2bd57391d2d2f353c933f75702e358a79a29fca
sergehould <houlds@vaniercollege.qc.ca>
2018-01-30 09:25:32 -0500

    Update lab1_main.cpp
    Comment added

--- win32_project2/lab1_main.cpp
+++ win32_project2/lab1_main.cpp
@@ -14,7 +14,7 @@ int main(void) {
 	set_color_depth(24);
 	set_gfx_mode(GFX_SAFE, 640, 480, 0, 0);
 	install_keyboard();
-	set_keyboard_rate(500, 600);
+	set_keyboard_rate(500, 600); // keyboard
 	textprintf_ex(screen, font, 1, 1, makecol(0,255,0),-1, "Hello World!");
 	textprintf_ex(screen, font, 1, 20, makecol(0,0,255), -1,"Press ESCape to quit");
 	while(! key[KEY_ESC]);
